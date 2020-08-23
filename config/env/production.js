4d2b2ffe98c7019f124ad62439a6ad78657220bd
Kelvin Omereshone <kelvinomereshone@gmail.com>
2020-08-23 18:15:36 +0100

    fix: sails web sockets

--- config/env/production.js
+++ config/env/production.js
@@ -227,10 +227,7 @@ module.exports = {
      * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
      *                                                                          *
      ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: ['https://logrocketsailsapi.herokuapp.com/'],
     /***************************************************************************
      *                                                                          *
      * If you are deploying a cluster of multiple servers and/or processes,     *
